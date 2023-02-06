f5016047d1e361c6ec154ff8905b075da114b34d
Jenil-Savla <you@example.com>
2023-02-06 21:29:06 +0530

    Updated response format

--- api/views.py
+++ api/views.py
@@ -22,16 +22,17 @@ class RegisterAPI(GenericAPIView):
 		#data['password'] = data['email'][:4]+str(random.randint(1000,9999))
 		data['password'] = '12345678'
 		serializer = self.serializer_class(data=data)
-		serializer.is_valid(raise_exception = True)
-		user = serializer.save()
-		token = Token.objects.create(user=user)
-		#current_site = get_current_site(request).domain
-		#relative_link = reverse('email-verify')
-		#link = 'http://'+current_site+relative_link+'?token='+ token.key
-		#data = {'email_body': f'Use this link to get verified {link}.\nUsername: {data['email']}\nPassword: {password}', 'subject':'Email Verification', 'to' : user.email}
-		#Util.send_email(data)
-		#return Response({'success':'success'},status=status.HTTP_201_CREATED)
-		return Response(serializer.data,status=status.HTTP_201_CREATED)
+		if serializer.is_valid(raise_exception = True):
+			user = serializer.save()
+			token = Token.objects.create(user=user)
+			#current_site = get_current_site(request).domain
+			#relative_link = reverse('email-verify')
+			#link = 'http://'+current_site+relative_link+'?token='+ token.key
+			#data = {'email_body': f'Use this link to get verified {link}.\nUsername: {data['email']}\nPassword: {password}', 'subject':'Email Verification', 'to' : user.email}
+			#Util.send_email(data)
+			#return Response({'success':'success'},status=status.HTTP_201_CREATED)
+			return Response({"status" : True ,"data" : serializer.data, "message" : 'Request Sent'},status=status.HTTP_200_OK)
+		return Response({"status" : False ,"data" : serializer.errors, "message" : "Failure"}, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginAPI(GenericAPIView):
 	
@@ -45,8 +46,8 @@ class LoginAPI(GenericAPIView):
 			#login(request,user)
 			serializer = self.serializer_class(user)
 			token = Token.objects.get(user=user)
-			return Response({'token' : token.key,'email' : user.email},status = status.HTTP_200_OK)
-		return Response('Invalid Credentials',status = status.HTTP_404_NOT_FOUND)
+			return Response({"status" : True ,"data" : {'token' : token.key,'email' : user.email}, "message" : 'Login Success'},status = status.HTTP_200_OK)
+		return Response({"status" : False ,"data" : {}, "message" : 'Invalid Credentials'},status = status.HTTP_401_UNAUTHORIZED)
 	
 class UserRequest(GenericAPIView):
 	
@@ -55,18 +56,24 @@ class UserRequest(GenericAPIView):
 	permission_classes = [permissions.IsAdminUser,]
 
 	def get(self,request):
-		users = self.get_queryset()
-		serializer = self.serializer_class(users, many=True)
-		return Response(serializer.data, status=status.HTTP_200_OK)
+		try:
+			users = self.get_queryset()
+			serializer = self.serializer_class(users, many=True)
+			return Response({"status" : True ,"data" : serializer.data, "message" : "Success"}, status=status.HTTP_200_OK)
+		except:
+			return Response({"status" : False ,"data" : {}, "message" : "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 	
 	def post(self,request):
-		email = request.data['email']
-		user = User.objects.get(email=email)
-		user_data = self.serializer_class(user).data
-		password = email[:4]+str(random.randint(1000,9999))
-		data ={'email_body': f"Congratulations! your account is activated.\nUsername: {user_data['email']}\nPassword: {password}", 'email_subject': "Account activated!",'to_email':user_data['email']}
-		util.send_email(data)
-		user.set_password(password)
-		user.is_active = True
-		user.save()
-		return Response({'success':'activated'}, status=status.HTTP_200_OK)
+		try:
+			email = request.data['email']
+			user = User.objects.get(email=email)
+			user_data = self.serializer_class(user).data
+			password = email[:4]+str(random.randint(1000,9999))
+			data ={'email_body': f"Congratulations! your account is activated.\nUsername: {user_data['email']}\nPassword: {password}", 'email_subject': "Account activated!",'to_email':user_data['email']}
+			util.send_email(data)
+			user.set_password(password)
+			user.is_active = True
+			user.save()
+			return Response({"status" : True ,"data" : {}, "message" : "User Activated"}, status=status.HTTP_200_OK)
+		except:
+			return Response({"status" : False ,"data" : {}, "message" : "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
